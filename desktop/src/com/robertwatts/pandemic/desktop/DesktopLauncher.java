ce9c1b70339077b44fe34334b2617d8898af8946
Robert Watts <mail@rlwatts.co.uk>
2020-10-23 14:42:01 +0100

    Create Project

---
+++ desktop/src/com/robertwatts/pandemic/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.robertwatts.pandemic.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.robertwatts.pandemic.PandemicGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PandemicGame(), config);
+	}
+}
